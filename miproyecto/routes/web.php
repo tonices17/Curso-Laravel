4775654a059023ab0c03627d23260f48ef756ba2
tonic <tonices17@gmail.com>
2024-03-07 10:26:31 +0100

    Hello World

--- miproyecto/routes/web.php
+++ miproyecto/routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'landing.index')->name('index');
+Route::view('/about', 'landing.about')->name('about');
